bfa8ed8740f432b434f8d0694035cf118c64b1c8
Vincent <vincent.k.97480@gmail.com>
2025-10-06 17:53:22 +0200

    [UPDATE] header home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <header className="flex items-center justify-between p-5">
+        <Image src="/icons/Logo.svg" alt="Logo" width={150} height={150} />
+        <nav className="mx-5">
+          <ul className="flex gap-5 [&>li]:bg-[var(--block)] [&>li]:px-4 [&>li]:py-2 [&>li]:rounded-sm [&>li]:cursor-pointer [&>li:hover]:bg-[var(--block-hover)]">
+            <li>Home</li>
+            <li>Collections</li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
